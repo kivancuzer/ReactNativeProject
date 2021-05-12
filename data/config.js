a97c67431cc18e51b0ccba7469f7674573c286e1
kivancuzer <kivancuzer@gmail.com>
2021-05-12 21:30:08 +0300

    base service implementation

---
+++ data/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiUrl:'https://northwind.vercel.app/',
+  
+}
+
+export default config;
---
+++ style/BaseService.js
@@ -0,0 +1,79 @@
+import config from '../data/config'
+
+const baseManager = {
+    get: async (url) => {
+        let responseData;
+
+        await fetch(config.apiUrl + url)
+            .then((res) => res.json())
+            .then((data) => {
+                responseData = data;
+            });
+
+        return responseData;
+    },
+
+    post: async (url, data) => {
+
+        let resultData;
+        let requestOptions = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
+        await fetch(config.apiUrl + url, requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                resultData = data;
+            });
+
+        return resultdata;
+
+    },
+
+    put: async (url, data)=>{
+        let resultData;
+        let requestOptions = {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
+        await fetch(config.apiUrl + url, requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                resultData = data;
+            });
+
+        return resultdata;
+    },
+
+    delete: async (url, id)=>{
+        let resultData;
+        let requestOptions = {
+            method: 'DELETE',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }
+
+        await fetch(config.apiUrl + url +"/"+id, requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                resultData = data;
+            });
+
+        return resultdata;
+    }
+
+
+}
