0fd29565813523e5006109ae27b0fe5d9b02cd47
abdulkadirakdeniz <abdulkadir.akdeniz@bahcesehir.edu.tr>
2021-05-14 04:48:53 +0300

    Indexpage screen changed

--- pages/IndexPage.js
+++ pages/IndexPage.js
@@ -5,21 +5,24 @@ import { Link } from 'react-router-dom'
 export default class IndexPage extends Component {
     render() {
         return (
-            <View style = {{flex : 3}}>
-              <View style={{ flex: 3, flexDirection: 'column' , margin : 10 , justifyContent:'space-around' }}>
-                <View style={{ flex: 1, backgroundColor: '#195082', justifyContent: 'center', alignItems: 'center'}}>
+            <View style = {{flex: 5}}>
+                <View style={{flex: 1, marginLeft:50, marginTop:50}}>
+                <Text style={{fontSize: 70, fontWeight:'300', fontStyle:'italic'}}>Choose one ..</Text>
+                </View>
+              <View style={{ flex: 4, flexDirection: 'column' , margin: 10 , justifyContent:' space-around' }}>
+                <View style={{ flex: 1, backgroundColor: '#F4BC02', justifyContent: 'center', alignItems: 'center', borderRadius: 250, margin: 50, marginHorizontal: 200}}>
                     <Link className="nav-link" to="/product-list" style={{textDecoration: 'none'}}>
-                        <Text style={{ color: '#F0F0F0', fontSize: 30, style: 'bold' }}>Products</Text>
+                        <Text style={{ color: '#F0F0F0', fontSize: 50, fontWeight:'900' }}>Products</Text>
                     </Link>
                 </View>
-                <View style={{ flex: 1, backgroundColor: '#195082', justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ flex: 1, backgroundColor: '#ff5338', justifyContent: 'center', alignItems: 'center' ,borderRadius: 250,margin: 50, marginHorizontal: 200}}>
                     <Link className="nav-link" to="/category-list" style={{textDecoration: 'none'}}>
-                        <Text style={{ color: '#F0F0F0', fontSize: 30, style: 'bold' }}>Categories</Text>
+                        <Text style={{ color: '#F0F0F0', fontSize: 50, fontWeight:'900' }}>Categories</Text>
                     </Link>
                 </View>
-                <View style={{ flex: 1, backgroundColor: '#195082', justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ flex: 1, backgroundColor: '#4391CE', justifyContent: 'center', alignItems: 'center' ,borderRadius: 250, margin: 50, marginHorizontal: 200}}>
                     <Link className="nav-link" to="/order-list" style={{textDecoration: 'none'}}>
-                        <Text style={{ color: '#F0F0F0', fontSize: 30 , style: 'bold'}}>Orders</Text>
+                        <Text style={{ color: '#F0F0F0', fontSize: 50 , fontWeight:'900'}}>Orders</Text>
                     </Link>
                 </View>
             </View>
