28316e4ff3fb7deff957b56c271aa8fe4c90727a
abdulkadirakdeniz <abdulkadir.akdeniz@bahcesehir.edu.tr>
2021-06-05 02:29:20 +0300

    AddCategoryForm page deleted
    Signed-off-by: abdulkadirakdeniz <abdulkadir.akdeniz@bahcesehir.edu.tr>

--- pages/AddCategoryPage.js
+++ pages/AddCategoryPage.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-import Basic from '../components/AddCategoryForm'
-import AddCategoryForm from '../components/AddCategoryForm'
 
 
 export default class AddCategoryPage extends Component {
     render() {
         return (
             <View>
-                <Basic/>
             </View>
         )
     }
